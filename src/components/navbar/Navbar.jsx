534c6d04c9cf2c66c549895252c79ff609ead905
umitd111 <umit.demirci1996@gmail.com>
2024-04-22 23:48:49 +0300

    responsive menu is created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
@@ -18,6 +20,21 @@ const Navbar = () => {
         <a href="/" className="register">
           Sign up
         </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
