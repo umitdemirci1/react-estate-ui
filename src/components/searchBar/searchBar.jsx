fdeb6c4ef2343e2e3c916ffd81cb3aa91f120060
umitd111 <umit.demirci1996@gmail.com>
2024-04-24 02:07:04 +0300

    responsive searchbar is created

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,9 +1,55 @@
-import './searchBar.scss';
+import "./searchBar.scss";
+import { useState } from "react";
+
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
-    return ( 
-        <div>Searchbar</div>
-     );
-}
- 
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (type) => {
+    setQuery((prev) => ({ ...prev, type: type }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="location"
+          min={0}
+          max={1000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="location"
+          min={0}
+          max={1000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default SearchBar;
