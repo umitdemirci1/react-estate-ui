2ed0f035d584c944f8ca0d1d9c03798f3bcecd13
umitd111 <umit.demirci1996@gmail.com>
2024-04-24 21:34:08 +0300

    react router dom installed. navbar layout is added. page design is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import Home from "./pages/home/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import List from "./pages/list/list";
+import Layout from "./pages/layout/layout";
+import SinglePage from "./pages/singlePage/singlePage";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/list",
+          element: <List />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
